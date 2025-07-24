74114acb0cb605cd31a732125a301863b727068c
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-07-24 18:30:10 +0600

    Transfer Data from Views to Controllers 1)Controllers Request Property – Request.Form

--- UnderstandingControllersViews/Controllers/HomeController.cs
+++ UnderstandingControllersViews/Controllers/HomeController.cs
@@ -6,27 +6,15 @@ namespace UnderstandingControllersViews.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult ReceivedDataByRequest()
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            string name = Request.Form["name"];
+            string sex = Request.Form["sex"];
+            return View("ReceivedDataByRequest", $"{name} sex is {sex}");
         }
     }
 }
