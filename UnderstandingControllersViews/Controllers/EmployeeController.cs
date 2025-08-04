a99d3324d8191541b6b6b2e3606cd4a38a9dc335
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-04 14:53:58 +0600

    Action Methods returning Class type Model

--- UnderstandingControllersViews/Controllers/EmployeeController.cs
+++ UnderstandingControllersViews/Controllers/EmployeeController.cs
@@ -1,18 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UnderstandingControllersViews.Models;
 
 namespace UnderstandingControllersViews.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult Index(int id, string name)
+        //{
+        //    string welcomeMessage = $"Welcome Employee: {name} with id :{id}";
+        //    return View((object)welcomeMessage);
+        //}
+        public IActionResult Detail()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index(int id, string name)
+        public IActionResult Detail(int id , string name )
         {
-            string welcomeMessage = $"Welcome Employee: {name} with id :{id}";
-            return View((object)welcomeMessage);
+            Employee emp = new Employee();
+            emp.Id = id;
+            emp.Name = name;
+            emp.Salary = 1000;
+            emp.Degignation = "Manager";
+            emp.Address = "Bangladesh";
+            return View(emp);
         }
     }
 }
