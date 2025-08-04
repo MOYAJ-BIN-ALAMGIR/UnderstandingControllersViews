99bd2f01db9e821ba94f58e5a8b90a62ba0f0c95
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-04 12:50:24 +0600

    Actions in ASP.NET Core
    Action Methods returning String type Model

---
+++ UnderstandingControllersViews/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnderstandingControllersViews.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int id, string name)
+        {
+            string welcomeMessage = $"Welcome Employee: {name} with id :{id}";
+            return View((object)welcomeMessage);
+        }
+    }
+}
