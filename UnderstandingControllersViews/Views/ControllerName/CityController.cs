22a1fa9ca4099ebc38fc42a63abe624436dd202c
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-05 22:07:13 +0600

    How to create a View in ASP.NET Core

---
+++ UnderstandingControllersViews/Views/ControllerName/CityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnderstandingControllersViews.Views.ControllerName
+{
+    public class CityController : Controller
+    {
+        public IActionResult NewYork()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
