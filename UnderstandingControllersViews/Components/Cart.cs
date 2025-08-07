a30d54a7134a2b49868c4bea76076efc31637c2c
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-07 12:20:03 +0600

    ASP.NET Core View Components
    Example: Simple View Component

---
+++ UnderstandingControllersViews/Components/Cart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnderstandingControllersViews.Components
+{
+    public class Cart:ViewComponent
+    {
+        public string Invoke()
+        {
+            return "This is from View Component";
+        }
+    }
+}
